2f2a41f4f5a9b6dcc1639b26092451284e60971e
MacTooKevin <53493098+MacTooKevin@users.noreply.github.com>
2019-10-31 01:59:58 +0300

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,236 @@
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>kids tech</title>
+        <link rel="shortcut icon" href="images/favicon.png" />
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/linearicons-free.css">
+        <link rel="stylesheet" href="css/simple-line-icons.css">
+        <link rel="stylesheet" href="vendor/fancybox/css/fancybox.css">
+        <link rel="stylesheet" href="css/global.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/kindergarten.css">
+    </head>
+    <body>
+
+        <div class="header_meta header_meta_one">
+            <div class="container">
+                <div class="row">
+                    <ul class="social">
+                        <li><a href="http://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="http://facebook.com/"><i class="fa fa-facebook-square"></i></a></li>
+                        <li><a href="http://linkedin.com/"><i class="fa fa-linkedin-square"></i></a></li>
+                        <li><a href="http://pinterest.com/"><i class="fa fa-pinterest"></i></a></li>
+                        <li><a href="http://instagram.com/"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="http://youtube.com/"><i class="fa fa-youtube"></i></a></li>
+                    </ul>
+                    <nav class="meta-login">
+                    <ul>
+                        <li class="clock"><i class="lnr lnr-clock"></i>Mon - Fri : 8:00 - 05:00</li>
+                        <li class="call"><i class="lnr lnr-smartphone"></i>Call Us +254 700 063 323</li>
+                        <!--li><a href="login.html" class="fancybox">Login</a></li-->
+                        <li><a href="signup.php" class="fancybox">Signup</a></li>
+                    </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+<header class="site-header home-one" id="masthead">
+    <div class="container">
+        <div class="row">
+
+            <div class="site-branding col-xs-12 col-sm-3"> <a href="index.html"><img alt="logo" class="brand" src="images/logo6.png" /></a> </div>
+
+
+            <nav class="main-navigation navbar navbar-default" id="site-navigation">
+                <div class="navbar-header">
+                    <button data-target=".navbar-ex1-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                    </button>
+                </div>
+                    <div class="collapse navbar-collapse navbar-ex1-collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item">
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="about.html">About us</a>
+                            </li>
+                            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" href="register.html">Programs<span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="bootcamp.html">Technology Bootcamps</a></li>
+                                    <li><a href="bootcamp.html">Code club events</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="active" href="contactus.html">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+        </div>
+
+    </div>
+
+    </header>
+    <div class="breadcrumb-section">
+        <div class="container">
+            <div class="row">
+                <header class="entry-header">
+                    <h1 class="entry-title">Contact Us</h1>
+                </header>
+            </div>
+        </div>
+    </div>
+    <div class="page-spacer clearfix">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 contact-form">
+                    <h3>Stay in Touch</h3>
+                    <p>leave a comment.
+                    </p>
+                    <div id="success" class="alert alert-success">Your message succesfully sent!</div>
+                    <div id="error" class="alert alert-danger">Opps! There is something wrong. Please try again</div>
+                    <form class="contactform" method="post" action="MAILTO:sylvia@iearnkenya.org" enctype="text/plain" id="contactForm">
+                        <p><input type="text" placeholder="First Name" required id="fname" name="fname"></p>
+                        <p><input type="text" placeholder="Last Name" required id="lname" name="lname"></p>
+                        <p><input type="text" id="phone" name="phone" placeholder="Phone Number"></p>
+                        <p><input type="email" id="email" name="email" required placeholder="Email Address"></p>
+                        <p><textarea placeholder="Message" name="message" id="message" rows="10" cols="40"></textarea></p>
+                        <p><input type="submit" class="btn btn-default" value="submit now">
+                    </form>
+                </div>
+
+
+                <div class="col-xs-12 col-sm-6 contact-info">
+                    <div class="col-xs-12 col-sm-6">
+                        <h3 class="head-border-default">Kids Tech</h3>
+                        <p class="address"><i class="fa fa-map-marker"></i>Ngara Road
+                            Figtree, next to Kenya police SACCO Plaza
+                                     Opposite Tea Zone
+                        </p>
+                        <p class="email"><i class="lnr lnr-envelope"></i><a>sylvia@iearnkenya.org</a></p>
+                        <ul class="social-icons">
+                            <li><a href="contactus.html#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="contactus.html#"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="contactus.html#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="contactus.html#"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <h3 class="head-border-default">Customer Supprt</h3>
+                        <p class="phone"><i class="fa fa-phone"></i>1234567890</p>
+                        <p class="email"><i class="lnr lnr-envelope"></i><a>programs@iearnkenya.org</a></p>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 have-question">
+                        <h3>Have Any Question?</h3>
+                        <p>feel free to ask any question and we wil contact you as soon as possible.</p>
+                        <a href="contactus.html#" class="btn btn-blue btn-medium">Ask a Question</a>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 map-location">
+                    <h3 class="head-border-default">Map and Location</h3>
+
+                    <div id="mapBox" class="row m0" data-lat="37.3818288" data-lon="-122.0636325" data-zoom="15" data-marker="images/map-marker.html"><p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8303445260367!2d36.81812401475392!3d-1.2750968990699796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f178b126d5147%3A0x3ba5f68af4728555!2sInternational+Education+and+Skills+Centre!5e0!3m2!1ssw!2ske!4v1563705222332!5m2!1ssw!2ske" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></p></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="call-to-action">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="col-xs-12 col-sm-8">
+                        <h3>Our Mission</h3>
+                        <p>To champion working and learning together and inspire discovery and invention through research, craetion and innovation<br><br>We don't just learn it we do and invent it.</p>
+                    </div>
+                        <a href="about.html" class="btn btn-medium btn-default pull-right">Know More <i class="lnr lnr-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="site-footer" id="colophon">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4"><img src="images/logo5.png" alt="" /></div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 footer-widget">
+                    <aside class="widget widget_text">
+                        <div class="textwidget">
+                            <p>iEARN is a non-profit organization made up of over 30,000 schools and youth organizations in more than 140 countries. iEARN empowers teachers and young people to work together online using the Internet and other new communications technologies. Over 2,000,000 students each day are engaged in collaborative project work worldwide.</p>
+                        </div>
+                    </aside>
+                    <aside class="widget socialmedia-widget">
+                        <ul class="social">
+                            <li><a href="http://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="http://facebook.com/"><i class="fa fa-facebook-square"></i></a></li>
+                            <li><a href="http://linkedin.com/"><i class="fa fa-linkedin-square"></i></a></li>
+                            <li><a href="http://pinterest.com/"><i class="fa fa-pinterest"></i></a></li>
+                            <li><a href="http://instagram.com/"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="http://youtube.com/"><i class="fa fa-youtube"></i></a></li>
+                        </ul>
+                    </aside>
+                </div>
+                 <div class="col-xs-12 col-sm-4 footer-widget">
+                    <aside class="widget widget_nav_menu">
+                        <ul class="menu">
+                            <li><a href="home.html#">Home</a></li>
+                            <li><a href="about.html">about US</a></li>
+                            <li><a href="contactus.html">Contact US</a></li>
+                            <li><a href="bootcamp.html">programs</a></li>
+                        </ul>
+                    </aside>
+                </div>
+                <div class="col-xs-12 col-sm-4 footer-widget">
+                    <aside class="widget newsletter-widget">
+                        <h3>Sign up now</h3>
+                        <p>If you want to book a space for the august bootcamp.</p>
+                        <a href="signup.php"><form id="newsletter">
+                          <input type="email" name="email" class="form-control" placeholder="Email" />
+                          <button type="submit" value="" class="btn btn-orange btn-medium"><i class="lnr lnr-arrow-right"></i></button>
+                      </a></form>
+                    </aside>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="copyright">
+
+                        <p class="develop col-xs-12 col-sm-4 text-right"><a target="_blank" href="https://www.codeclub.africa">&copy;2019Kids Tech</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/theme.js"></script>
+
+    <script type="text/javascript" src="vendor/fancybox/js/jquery.fancybox.js"></script>
+    <script type="text/javascript" src="vendor/fancybox/js/custom.fancybox.js"></script>
+    <script>
+        jQuery.noConflict();
+        jQuery('.fancybox').fancybox();
+    </script>
+    </body>
+</html>
